bcbb931e396954d1d00fc9464854f0b24e83dba0
Parsa <ParsaRajabiPR@gmail.com>
2021-04-10 22:07:54 -0700

    webwork script: added code to create .md file from python script

--- webwork/webwork_to_md.py
+++ webwork/webwork_to_md.py
@@ -13,3 +13,45 @@ for item in file_contents.split("\n"):
         title = item[item.find("(") + 1:item.find(")")].replace("'", "")
     if "## AuthorText1" in item:
         author = item[item.find("(") + 1:item.find(")")].replace("'", "")
+
+# problem text
+problem_text = "Problem Text TBD\n"
+
+# answer
+answer_section = "Solution Text TBD"
+# Preparing the YAML dictionary
+
+# This solution is copied from this SO answer: https://stackoverflow.com/a/45004775/2217577
+yaml.SafeDumper.org_represent_str = yaml.SafeDumper.represent_str
+
+
+def repr_str(dumper, data):
+    if '\n' in data:
+        return dumper.represent_scalar(u'tag:yaml.org,2002:str', data, style='|')
+    return dumper.org_represent_str(data)
+
+
+yaml.add_representer(str, repr_str, Dumper=yaml.SafeDumper)
+
+source = "https://github.com/openwebwork/webwork-open-problem-library"
+yaml_dict = {}
+
+yaml_dict['title'] = title
+yaml_dict['author'] = author
+yaml_dict['source'] = source
+yaml_dict['type'] = problem_type
+yaml_dict['tags'] = ['TBD']
+yaml_dict['outcomes'] = ['TBD']
+yaml_dict['assets'] = ['TBD']
+# yaml_dict['server'] = full_python #'import random \\n b=u'
+# pprint(yaml_dict)
+print(yaml.safe_dump(yaml_dict, sort_keys=False))
+
+start = "# {{ vars.title }}\n\n## Question Text\n\n"
+
+Path("test_question.md").write_text('---\n' + \
+                                    yaml.safe_dump(yaml_dict, sort_keys=False) + \
+                                    '---\n' + \
+                                    start + \
+                                    problem_text + \
+                                    answer_section)
