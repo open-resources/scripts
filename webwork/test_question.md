b426876bbde1bcbfc4a45d65690bb78024cb7eee
Parsa <ParsaRajabiPR@gmail.com>
2021-04-10 22:41:31 -0700

    webwork script: updated variable initialization, added comments, extracted tags and date from file for metadata

--- webwork/test_question.md
+++ webwork/test_question.md
@@ -1,10 +1,14 @@
 ---
 title: College Physics
 author: Urone et. al
+date: January 2018
+editor: Wynne Reichheld, April 30, 2018
 source: https://github.com/openwebwork/webwork-open-problem-library
 type: ''
 tags:
-- TBD
+- Mechanics
+- Kinematics
+- Time, Velocity, and Speed
 outcomes:
 - TBD
 assets:
--- webwork/webwork_to_md.py
+++ webwork/webwork_to_md.py
@@ -1,26 +1,46 @@
 from pathlib import Path
 import yaml
 
+# open file
 problem_file_name = 'NU_U17-2-03-005.pg'  # short answer
 path = '../../webwork-open-problem-library/Contrib/BrockPhysics/College_Physics_Urone/2.Kinematics/' + problem_file_name
-
 file = open(path, 'r')
 file_contents = file.read()
-title = author = editor = source = problem_type = tags = outcomes = assets = server = ""
+
+# declare variables
+
+metadata = "## "
+title_src = "TitleText1"
+author_src = "AuthorText1"
+editor_src = "Editor"
+db_src = "DB"
+date_src = "Date"
+
+title = author = editor = date = source = problem_type = outcomes = assets = server = ""
+tags = []
+
+# get metadata from file
 
 for item in file_contents.split("\n"):
-    if "## TitleText1" in item:
+    if metadata + title_src in item:
         title = item[item.find("(") + 1:item.find(")")].replace("'", "")
-    if "## AuthorText1" in item:
+    if metadata + author_src in item:
         author = item[item.find("(") + 1:item.find(")")].replace("'", "")
-    if "## Editor" in item:
+    if metadata + editor_src in item:
         editor = item[item.find("(") + 1:item.find(")")].replace("'", "")
+    if metadata + db_src in item:
+        tags.append(item[item.find("(") + 1:item.find(")")].replace("'", ""))
+    if metadata + date_src in item:
+        date = item[item.find("(") + 1:item.find(")")].replace("'", "")
 
-# problem text
+
+# get problem text from file
 problem_text = "Problem Text TBD\n"
 
-# answer
+# get answer from file
 answer_section = "Solution Text TBD"
+
+
 # Preparing the YAML dictionary
 
 # This solution is copied from this SO answer: https://stackoverflow.com/a/45004775/2217577
@@ -40,10 +60,11 @@ yaml_dict = {}
 
 yaml_dict['title'] = title
 yaml_dict['author'] = author
+yaml_dict['date'] = date
 yaml_dict['editor'] = editor
 yaml_dict['source'] = source
 yaml_dict['type'] = problem_type
-yaml_dict['tags'] = ['TBD']
+yaml_dict['tags'] = tags
 yaml_dict['outcomes'] = ['TBD']
 yaml_dict['assets'] = ['TBD']
 # yaml_dict['server'] = full_python #'import random \\n b=u'
